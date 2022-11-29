0381a597abb8b46e523c82457b542ffe80b3c00d
Val <valrijana@gmail.com>
2022-11-29 17:26:53 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuse();
+  });
+};
+let excuse = () => {
+  let who = ["The cat", "My little bro", "His puppy", "My turtle"];
+  let what = ["ate", "vomited", "smashed", "broke"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
 };
